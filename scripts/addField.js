5902e2ca62c369eb0b2fc3da0db29d12f9fcbcaa
Guilherme Souza <dev.guilhermesouza@gmail.com>
2020-08-05 21:20:38 -0300

    Dia 3 finalizado

---
+++ scripts/addField.js
@@ -0,0 +1,25 @@
+const addTimeBtn = document.querySelector('#add-time');
+const scheduleItems = document.querySelector('#schedule-items');
+const scheduleItem = document.querySelector('.schedule-item');
+
+const addTimeRemover = parentElement => {
+	const timeRemover = document.createElement('span');
+	timeRemover.textContent = 'Remover';
+	timeRemover.onclick = () => timeRemover.parentNode.remove();
+	timeRemover.classList.add('time-remover');
+	parentElement.appendChild(timeRemover);
+}
+
+const cloneField = () => {
+	const newFieldContainer = scheduleItem.cloneNode(true);
+
+	const fields = newFieldContainer.querySelectorAll('input');
+
+	fields.forEach(field => field.value = '');
+
+	addTimeRemover(newFieldContainer);
+
+	scheduleItems.appendChild(newFieldContainer);
+};
+
+addTimeBtn.addEventListener('click', cloneField);
