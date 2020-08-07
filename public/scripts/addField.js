4db1ff8529cf0b413aa5bca899d66f6bbb2ed20a
Guilherme Souza <dev.guilhermesouza@gmail.com>
2020-08-07 15:48:51 -0300

    Dia 5 finalizado

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -4,19 +4,19 @@ const scheduleItem = document.querySelector('.schedule-item');
 
 const addTimeRemover = parentElement => {
 	const timeRemover = document.createElement('span');
-	timeRemover.textContent = 'Remover';
+
+	timeRemover.textContent = '- Remover';
 	timeRemover.onclick = () => timeRemover.parentNode.remove();
+
 	timeRemover.classList.add('time-remover');
 	parentElement.appendChild(timeRemover);
 }
 
 const cloneField = () => {
 	const newFieldContainer = scheduleItem.cloneNode(true);
-
 	const fields = newFieldContainer.querySelectorAll('input');
 
 	fields.forEach(field => field.value = '');
-
 	addTimeRemover(newFieldContainer);
 
 	scheduleItems.appendChild(newFieldContainer);
--- src/server.js
+++ src/server.js
@@ -1,90 +1,13 @@
-// Dados
-
-const proffys = [
-	{
-		name: "Diego Fernandes",
-		avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-		whatsapp: "89987654534",
-		bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-		subject: "Química",
-		cost: "20,00",
-		weekday: [0],
-		time_from: [720],
-		time_to: [1220]
-	},
-	{
-		name: "Mayk Brito",
-		avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-		whatsapp: "89987654534",
-		bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-		subject: "Educação física",
-		cost: "40,00",
-		weekday: [1],
-		time_from: [720],
-		time_to: [1220]
-	},
-]
-
-const subjects = [
-	"Artes",
-	"Biologia",
-	"Ciências",
-	"Educação física",
-	"Física",
-	"Geografia",
-	"História",
-	"Matemática",
-	"Português",
-	"Química",
-]
-
-const weekdays = [
-	"Domingo",
-	"Segunda-feira",
-	"Terça-feira",
-	"Quarta-feira",
-	"Quinta-feira",
-	"Sexta-feira",
-	"Sábado",
-]
-// Funcionalidades
-
-function getSubject(subjectValue){
-	const arrayPosition = +subjectValue-1
-	return subjects[arrayPosition];
-}
-
-function pageLanding(req, res){
-	return res.render('index.html')
-}
-
-function pageStudy(req, res){
-	const filters = req.query
-	return res.render('study.html', { proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res){
-	const data = req.query
-	// Adicionar dados a lista de proffys
-
-	console.log(data);
-
-	const isNotEmpty = Object.keys(data).length > 0;
-
-	if (isNotEmpty){
-		data.subject = getSubject(data.subject)
-
-		proffys.push(data)	
-		return res.redirect('/study')
-	}else{
-		return res.render('give-classes.html', { subjects, weekdays })
-	}
-}
-
-
 // Servidor
 const express = require('express')
 const server = express()
+
+const {
+	pageLanding,
+	pageStudy,
+	pageGiveClasses,
+	saveClasses
+} = require('./pages')
 
 // Configuração do Nunjucks (template engine)
 const nunjucks = require('nunjucks')
@@ -95,10 +18,13 @@ nunjucks.configure('src/views', {
 
 // Início e configuração do servidor
 server
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
 // Configuração de arquivos estáticos
 	.use(express.static("public"))
 // Rotas da aplicação
 	.get('/', pageLanding)
 	.get('/study', pageStudy)
 	.get('/give-classes', pageGiveClasses)
+	.post('/save-classes', saveClasses)
 	.listen(5500)
